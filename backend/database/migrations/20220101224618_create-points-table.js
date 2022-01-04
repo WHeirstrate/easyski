b212775eaa8c21e0cbbb8fcd57e0dc9222799b77
Wouter Heirstrate <wouter.heirstrate@gmail.com>
2022-01-04 19:53:29 +0100

    small fixes

--- backend/database/migrations/20220101224618_create-points-table.js
+++ backend/database/migrations/20220101224618_create-points-table.js
@@ -1,7 +1,13 @@
 exports.up = async function (KNEX) {
   return await KNEX.schema.createTable("points", async (table) => {
-    table.increments("id");
-    table.uuid("uuid").defaultTo(KNEX.raw("gen_random_uuid()")).notNullable();
+    //table.primary(["id", "uuid"]);
+    //table.increments("id");
+    table.specificType("id", "serial");
+    table
+      .uuid("uuid")
+      .notNullable()
+      .defaultTo(KNEX.raw("gen_random_uuid()"))
+      .primary();
     table.integer("area_id").defaultTo(1).notNullable();
     table.double("latitude").notNullable();
     table.double("longtitude").notNullable();
--- backend/database/migrations/20220104102114_create-tracks-table.js
+++ backend/database/migrations/20220104102114_create-tracks-table.js
@@ -1,7 +1,13 @@
 exports.up = async function (KNEX) {
   return await KNEX.schema.createTable("tracks", async (table) => {
-    table.increments("id");
-    table.uuid("uuid").defaultTo(KNEX.raw("gen_random_uuid()")).notNullable();
+    //table.primary(["id", "uuid"]);
+    //table.increments("id");
+    table.specificType("id", "serial");
+    table
+      .uuid("uuid")
+      .notNullable()
+      .defaultTo(KNEX.raw("gen_random_uuid()"))
+      .primary();
     table.integer("tracktype_id").defaultTo(1).notNullable();
     table.string("name").notNullable();
     table.boolean("slope").defaultTo(true).notNullable();
